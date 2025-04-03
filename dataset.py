345e88a8214752df160fc306bf0e63f90a7fdc70
huskywannacry <ttkien1503@gmail.com>
2025-04-03 08:16:21 +0700

    final version

--- dataset.py
+++ dataset.py
@@ -1,148 +1,85 @@
-"""
-Main file for training Yolo model on Pascal VOC dataset
-
-"""
-
 import torch
-import torchvision.transforms as transforms
-import torch.optim as optim
-import torchvision.transforms.functional as FT
-from tqdm import tqdm
-from torch.utils.data import DataLoader
-from model import Yolov1
-from dataset import VOCDataset
-from utils import (
-    non_max_suppression,
-    mean_average_precision,
-    intersection_over_union,
-    cellboxes_to_boxes,
-    get_bboxes,
-    plot_image,
-    save_checkpoint,
-    load_checkpoint,
-)
-from loss import YoloLoss
-
-seed = 123
-torch.manual_seed(seed)
-
-# Hyperparameters etc. 
-LEARNING_RATE = 2e-5
-DEVICE = "cuda" if torch.cuda.is_available else "cpu"
-BATCH_SIZE = 16 # 64 in original paper but I don't have that much vram, grad accum?
-WEIGHT_DECAY = 0
-EPOCHS = 1000
-NUM_WORKERS = 2
-PIN_MEMORY = True
-LOAD_MODEL = False
-LOAD_MODEL_FILE = "overfit.pth.tar"
-IMG_DIR = "data/images"
-LABEL_DIR = "data/labels"
+import os
+import pandas as pd
+from PIL import Image
 
 
-class Compose(object):
-    def __init__(self, transforms):
-        self.transforms = transforms
+class VOCDataset(torch.utils.data.Dataset):
+    def __init__(
+        self, csv_file, img_dir, label_dir, S=7, B=2, C=20, transform=None,
+    ):
+        self.annotations = pd.read_csv(csv_file)
+        self.img_dir = img_dir
+        self.label_dir = label_dir
+        self.transform = transform
+        self.S = S
+        self.B = B
+        self.C = C
 
-    def __call__(self, img, bboxes):
-        for t in self.transforms:
-            img, bboxes = t(img), bboxes
+    def __len__(self):
+        return len(self.annotations)
 
-        return img, bboxes
+    def __getitem__(self, index):
+        label_path = os.path.join(self.label_dir, self.annotations.iloc[index, 1])
+        boxes = []
+        with open(label_path) as f:
+            for label in f.readlines():
+                class_label, x, y, width, height = [
+                    float(x) if float(x) != int(float(x)) else int(x)
+                    for x in label.replace("\n", "").split()
+                ]
 
+                boxes.append([class_label, x, y, width, height])
 
-transform = Compose([transforms.Resize((448, 448)), transforms.ToTensor(),])
+        img_path = os.path.join(self.img_dir, self.annotations.iloc[index, 0])
+        image = Image.open(img_path)
+        boxes = torch.tensor(boxes)
 
+        if self.transform:
+            image, boxes = self.transform(image, boxes)
 
-def train_fn(train_loader, model, optimizer, loss_fn):
-    loop = tqdm(train_loader, leave=True)
-    mean_loss = []
+        # Convert To Cells
+        label_matrix = torch.zeros((self.S, self.S, self.C + 5 * self.B))
+        for box in boxes:
+            class_label, x, y, width, height = box.tolist()
+            class_label = int(class_label)
 
-    for batch_idx, (x, y) in enumerate(loop):
-        x, y = x.to(DEVICE), y.to(DEVICE)
-        out = model(x)
-        loss = loss_fn(out, y)
-        mean_loss.append(loss.item())
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
+            # i,j represents the cell row and cell column
+            i, j = int(self.S * y), int(self.S * x)
+            x_cell, y_cell = self.S * x - j, self.S * y - i
 
-        # update progress bar
-        loop.set_postfix(loss=loss.item())
+            """
+            Calculating the width and height of cell of bounding box,
+            relative to the cell is done by the following, with
+            width as the example:
+            
+            width_pixels = (width*self.image_width)
+            cell_pixels = (self.image_width)
+            
+            Then to find the width relative to the cell is simply:
+            width_pixels/cell_pixels, simplification leads to the
+            formulas below.
+            """
+            width_cell, height_cell = (
+                width * self.S,
+                height * self.S,
+            )
 
-    print(f"Mean loss was {sum(mean_loss)/len(mean_loss)}")
+            # If no object already found for specific cell i,j
+            # Note: This means we restrict to ONE object
+            # per cell!
+            if label_matrix[i, j, 20] == 0:
+                # Set that there exists an object
+                label_matrix[i, j, 20] = 1
 
+                # Box coordinates
+                box_coordinates = torch.tensor(
+                    [x_cell, y_cell, width_cell, height_cell]
+                )
 
-def main():
-    model = Yolov1(split_size=7, num_boxes=2, num_classes=20).to(DEVICE)
-    optimizer = optim.Adam(
-        model.parameters(), lr=LEARNING_RATE, weight_decay=WEIGHT_DECAY
-    )
-    loss_fn = YoloLoss()
+                label_matrix[i, j, 21:25] = box_coordinates
 
-    if LOAD_MODEL:
-        load_checkpoint(torch.load(LOAD_MODEL_FILE), model, optimizer)
+                # Set one hot encoding for class_label
+                label_matrix[i, j, class_label] = 1
 
-    train_dataset = VOCDataset(
-        "data/100examples.csv",
-        transform=transform,
-        img_dir=IMG_DIR,
-        label_dir=LABEL_DIR,
-    )
-
-    test_dataset = VOCDataset(
-        "data/test.csv", transform=transform, img_dir=IMG_DIR, label_dir=LABEL_DIR,
-    )
-
-    train_loader = DataLoader(
-        dataset=train_dataset,
-        batch_size=BATCH_SIZE,
-        num_workers=NUM_WORKERS,
-        pin_memory=PIN_MEMORY,
-        shuffle=True,
-        drop_last=True,
-    )
-
-    test_loader = DataLoader(
-        dataset=test_dataset,
-        batch_size=BATCH_SIZE,
-        num_workers=NUM_WORKERS,
-        pin_memory=PIN_MEMORY,
-        shuffle=True,
-        drop_last=True,
-    )
-
-    for epoch in range(EPOCHS):
-        # for x, y in train_loader:
-        #    x = x.to(DEVICE)
-        #    for idx in range(8):
-        #        bboxes = cellboxes_to_boxes(model(x))
-        #        bboxes = non_max_suppression(bboxes[idx], iou_threshold=0.5, threshold=0.4, box_format="midpoint")
-        #        plot_image(x[idx].permute(1,2,0).to("cpu"), bboxes)
-
-        #    import sys
-        #    sys.exit()
-
-        pred_boxes, target_boxes = get_bboxes(
-            train_loader, model, iou_threshold=0.5, threshold=0.4
-        )
-
-        mean_avg_prec = mean_average_precision(
-            pred_boxes, target_boxes, iou_threshold=0.5, box_format="midpoint"
-        )
-        print(f"Train mAP: {mean_avg_prec}")
-
-        #if mean_avg_prec > 0.9:
-        #    checkpoint = {
-        #        "state_dict": model.state_dict(),
-        #        "optimizer": optimizer.state_dict(),
-        #    }
-        #    save_checkpoint(checkpoint, filename=LOAD_MODEL_FILE)
-        #    import time
-        #    time.sleep(10)
-
-        train_fn(train_loader, model, optimizer, loss_fn)
-
-
-if __name__ == "__main__":
-    main()
+        return image, label_matrix
